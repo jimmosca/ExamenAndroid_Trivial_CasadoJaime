fcd8a2542dabef40322b24e646c111ee3c697b2d
Jaime Casado <jaime.casado.a@gmail.com>
2019-11-22 11:21:27 +0100

    Root commit

---
+++ app/src/main/java/com/jca/examenandroid_trivial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jca.examenandroid_trivial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
